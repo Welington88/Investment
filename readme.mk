ef2911f728d1f9cf4dd285472b1a02c66892acd2
welingtonmarquezini <welington_marquezini88@live.com>
2024-07-30 14:43:27 -0300

    docs(mk) - ajuste no arquivos de comandos

--- readme.mk
+++ readme.mk
@@ -1,29 +1,25 @@
-# Nome do projeto
 PROJECT_NAME = InvestmentApp
 
-# Comandos
 install:
-    @echo "Instalando dependências em .net"
-    dotnet restore
+	@echo "Instalando dependências em .net"
+	cd src/Investment.View/ClientApp && npm install
 
-start:
-    @echo "Iniciando a aplicação Investment.API BackEnd"
-    dotnet run --project ./src/Investment.API  --launch-profile "https" 
+start-backend:
+	@echo "Iniciando a aplicação Investment.API BackEnd"
+	cd ./ && dotnet run --project ./src/Investment.API  --launch-profile "https" 
 
-start:
-    @echo "Executando a aplicação Investment.View FrontEnd"
-    dotnet run --project ./src/Investment.View
+start-frontend:
+	@echo "Iniciando a aplicação Investment.API Frontend"
+	cd ./ && dotnet run --project ./src/Investment.View	
 
 test:
-    @echo "Executando projeto de tests"
-    dotnet test ./tests/Investment.Tests
+	@echo "Executando projeto de tests"
+	dotnet test ./tests/Investment.Tests
 
 build:
-    @echo "Construindo a aplicação"
-    dotnet build
+	@echo "Construindo a aplicação"
+	dotnet build
 
 clean:
-    @echo "Limpando arquivos de build"
-    dotnet clean
-
-.DEFAULT_GOAL := help
+	@echo "Limpando arquivos de build"
+	dotnet clean
